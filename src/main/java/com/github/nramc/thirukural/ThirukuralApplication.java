aaa921f231695b77008d986ac4508a891a514718
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-06-30 21:44:39 +0200

    [chore] initial commit

---
+++ src/main/java/com/github/nramc/thirukural/ThirukuralApplication.java
@@ -0,0 +1,13 @@
+package com.github.nramc.thirukural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirukuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirukuralApplication.class, args);
+	}
+
+}
